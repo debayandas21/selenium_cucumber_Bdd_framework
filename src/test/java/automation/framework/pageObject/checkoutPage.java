a4dd57a047ad5aa293366bf8fa065a7b68cef107
debayandas21 <debayan.deb2007@gmail.com>
2025-02-21 12:21:30 +0530

    commit updated code

---
+++ src/test/java/automation/framework/pageObject/checkoutPage.java
@@ -0,0 +1,11 @@
+package automation.framework.pageObject;
+
+import org.openqa.selenium.WebDriver;
+
+public class checkoutPage {
+
+	public WebDriver driver;
+	public checkoutPage(WebDriver driver) {
+		this.driver=driver;
+	}
+}
